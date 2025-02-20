7739cda8f601c8fcf84d1a5410b456aefe90532f
Biswajit-naik <naik3797@gmail.com>
2025-02-20 19:28:32 +0530

    cacheconfig

--- spring/testserver/src/main/java/comt/est_server/testserver/configurations/Appconfig.java
+++ spring/testserver/src/main/java/comt/est_server/testserver/configurations/Appconfig.java
@@ -1,4 +1,15 @@
 package comt.est_server.testserver.configurations;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class Appconfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
+
